9b181f004d08d6006c61cff7bb48352034be76af
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2016-03-30 08:55:33 +0100

    Update problem 1

--- problems/001.js
+++ problems/001.js
@@ -1,4 +1,4 @@
 import { filter, range, sum, modulo } from 'ramda';
 
-const isMultiple = n => modulo(n, 3) === 0 || modulo(n, 5) === 0;
-console.log(sum(filter(isMultiple, range(1, 1000))));
+console.log(sum(filter(n => modulo(n, 3) === 0 ||
+                       modulo(n, 5) === 0, range(1, 1000))));
