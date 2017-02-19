4cf283f899c68bfd15c5a2f474af852d12ed6822
Ryan Waudby <ryan@ryanwaudby.co.uk>
2017-02-19 13:23:43 +0000

    Update problem 1

--- problems/001.js
+++ problems/001.js
@@ -1,4 +1,6 @@
-import { filter, range, sum, modulo } from 'ramda';
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
 
-console.log(sum(filter(n => modulo(n, 3) === 0 ||
-                       modulo(n, 5) === 0, range(1, 1000))));
+import { filter, range, sum, pipe } from 'ramda';
+
+pipe(range, filter(n => n % 3 === 0 || n % 5 === 0), sum, console.log)(1, 1000);
